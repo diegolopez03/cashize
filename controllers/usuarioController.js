29c123a40e68a509809c2f9818dca67a523e69c8
diegolopez03 <diego232.lopez@gmail.com>
2020-11-02 19:25:49 -0600

    Verficar que no existan errores de validación de campos, si los hay agregarlos a nuestros mensajes flash

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,6 +1,7 @@
 //Importar los módulos requeridos
 const mongoose = require("mongoose");
 const Usuario = mongoose.model("Usuarios");
+const { validationResult } = require("express-validator");
 
 //Cargar el formulario de creación de una cuenta de usuario
 exports.formularioCrearCuenta = (req, res, next) =>{
@@ -9,10 +10,29 @@ exports.formularioCrearCuenta = (req, res, next) =>{
 
 //Procesar el formulario de creación de cuenta 
 exports.crearCuenta = async (req, res, next) => {
+    //Verificar que no existan errores de validación
+    const errores = validationResult(req);
+    const erroresArray = [];
+    //Si hay errores
+    if(!errores.isEmpty()) {
+        //Utilizar la funcion map para navegar dentro de un arreglo
+        errores.array().map(error => erroresArray.push(error.msg));
+
+        console.log(erroresArray);
+
+        //\Agregar los errores a nuestros mensajes flash 
+        req.flash("error", erroresArray);
+
+        res.render("registrarse", {
+            layout: "auth",
+            messages: req.flash()
+        });
+    }
+
     //Obtener las variables desde el cuerpo de la peticón 
     const { nombre, email, password } = req.body;
 
-    //Intentar almacenar los datos del usuario
+    // //Intentar almacenar los datos del usuario
     try {
         //Crear el usuario
         await Usuario.create({
@@ -20,6 +40,7 @@ exports.crearCuenta = async (req, res, next) => {
             password,
             nombre
         });
+        res.redirect("/iniciar-sesion");
     } catch (error) {
         console.log(error);
     }
