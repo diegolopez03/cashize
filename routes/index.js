64fec4640bc9f6f1860fae31e13fd869615eb42d
diegolopez03 <diego232.lopez@gmail.com>
2020-11-02 19:26:44 -0600

    Implementación de express validator y agregarle validaciones a la ruta de registrarse

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const usuarioController = require("../controllers/usuarioController");
 const authController = require("../controllers/authController");
+const { check } = require("express-validator");
 
 //Configura y mantiene todos los endpoints en el servidor 
 const router = express.Router();
@@ -15,7 +16,23 @@ module.exports = () => {
     //Rutas para usuario
     router.get("/crear-cuenta", usuarioController.formularioCrearCuenta);
 
-    router.post("/registrarse", usuarioController.crearCuenta);
+    router.post("/registrarse",
+    [
+    //Realizar una verificación de los atributos del formulario
+    check("nombre", "Debes ingresar tu nombre.")
+    .not()
+    .isEmpty()
+    .escape(),
+    check("email", "Debes ingresar un correo electrónico.")
+    .not().isEmpty(),
+    check("email", "El correo electónico no es válido.")
+    .isEmail().normalizeEmail(),
+    check("password", "Debes ingresar una contraseña").not().isEmpty(),
+    //Validacion de campo de confirmacion de contraseña.
+    // check("confirmpassoword", "La contraseña de confirmación es diferente")
+    // .custom((value, { req }) => value === req.body.password),
+    ],
+    usuarioController.crearCuenta);
 
     router.get("/iniciar-sesion", usuarioController.formularioIniciarSesion);
 
